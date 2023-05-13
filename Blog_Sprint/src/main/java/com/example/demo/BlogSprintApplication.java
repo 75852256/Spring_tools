1b26cf0cdee3ec1449e7e7cf7716fa84b87e5d3d
Shiang Patrick Navarro Prado <81573026+75852256@users.noreply.github.com>
2023-05-13 10:08:41 -0500

    Add files via upload

---
+++ Blog_Sprint/src/main/java/com/example/demo/BlogSprintApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSprintApplication.class, args);
+		}
+}
+
+
